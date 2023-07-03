ec4a60fe8bbfee63e44451c19b96f41da8edcf3d
akshaybombale <akshaybombale70@gmail.com>
2023-07-03 17:51:41 +0530

    calender foe whole year

--- calender.py
+++ calender.py
@@ -1,7 +1,11 @@
 import calendar
 
-# Create a calendar for a specific year and month
+# Create a calendar for the whole year
 year = 2023
-month = 7
-calendar_text = calendar.month(year, month)
-print(calendar_text)
+
+# Iterate over the months and generate the calendar for each month
+for month in range(1, 13):
+    calendar_text = calendar.month(year, month)
+    print(f"{'-'*20} {calendar.month_name[month]} {'-'*20}")
+    print(calendar_text)
+    print()
